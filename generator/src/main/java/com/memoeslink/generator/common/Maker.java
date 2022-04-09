7544d63356a312dec9295b916226be5c8d1c48e1
memoeslink <memocad@gmail.com>
2022-04-09 10:18:15 -0500

    Fixed some minor bugs.

--- generator/src/main/java/com/memoeslink/generator/common/Maker.java
+++ generator/src/main/java/com/memoeslink/generator/common/Maker.java
@@ -1,12 +1,16 @@
 package com.memoeslink.generator.common;
 
+import android.graphics.Color;
+
 import com.memoeslink.common.Randomizer;
 
 public class Maker {
 
     public static String getColor(String s) {
+        if (StringHelper.isNullOrEmpty(s))
+            return String.format("#%06x", (0xFFFFFF & Color.GRAY));
         StringGenerator stringGenerator = new StringGenerator(null, LongHelper.getSeed(s));
-        return stringGenerator.getStrColor(s);
+        return stringGenerator.getStrColor();
     }
 
     public static String getDefaultColor(String s) {
--- generator/src/main/java/com/memoeslink/generator/common/StringGenerator.java
+++ generator/src/main/java/com/memoeslink/generator/common/StringGenerator.java
@@ -20,9 +20,7 @@ public class StringGenerator extends Generator {
         return String.format(locale, "%.2f%%", r.getFloat(0.0F, 100.0F));
     }
 
-    public String getStrColor(String s) {
-        if (StringHelper.isNullOrEmpty(s))
-            return String.format("#%06x", (0xFFFFFF & -7829368));
+    public String getStrColor() {
         return String.format("#%06x", r.getInt(0xFFFFFF + 1));
     }
 }
