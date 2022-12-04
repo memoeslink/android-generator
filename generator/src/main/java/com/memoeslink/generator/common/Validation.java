f438b5335c1719d9727e952d129e1b693a0554a8
Memo <memocad@gmail.com>
2022-12-04 15:12:12 -0600

    Fixed wrong regex for hex colors.

--- generator/src/main/java/com/memoeslink/generator/common/Validation.java
+++ generator/src/main/java/com/memoeslink/generator/common/Validation.java
@@ -8,7 +8,7 @@ import java.util.regex.Pattern;
 public class Validation {
     private static final String PHONE_REGEX = "^(\\+\\d{1,3}( )?)?((\\(\\d{1,3}\\))|\\d{1,3})[- .]?\\d{3,4}[- .]?\\d{4}$";
     private static final Pattern PHONE_PATTERN = Pattern.compile(PHONE_REGEX);
-    private static final String HEX_COLOR_REGEX = "^((a|b|big|i|s|small|tt|u),)*(a|b|big|i|s|small|tt|u)$";
+    private static final String HEX_COLOR_REGEX = "^#([0-9a-f]{3}|[0-9a-f]{6}|[0-9a-f]{8})$";
     private static final Pattern HEX_COLOR_PATTERN = Pattern.compile(HEX_COLOR_REGEX);
 
     public static boolean isEmailAddress(String email) {
