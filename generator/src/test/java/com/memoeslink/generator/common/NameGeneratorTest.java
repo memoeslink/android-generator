97f895d62760b8fff320938a0f5cd98e9a17fa9d
memoeslink <memoeslink@hotmail.com>
2023-07-31 10:44:07 -0600

    Changed build.gradle. Added NameGeneratorTest.java to test common/NameGenerator.java.

---
+++ generator/src/test/java/com/memoeslink/generator/common/NameGeneratorTest.java
@@ -0,0 +1,27 @@
+package com.memoeslink.generator.common;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.memoeslink.StringHelper;
+
+public class NameGeneratorTest {
+
+    @Test
+    public void getNameOrDefault() throws Exception {
+        NameGenerator nameGenerator = new NameGenerator();
+        Assertions.assertNotEquals(StringHelper.EMPTY, nameGenerator.getNameOrDefault(NameType.EMPTY, "John Doe"));
+    }
+
+    @Test
+    public void getNameOrRetry() throws Exception {
+        NameGenerator nameGenerator = new NameGenerator();
+        String name = nameGenerator.getNameOrRetry(NameType.DOUBLE_BARRELLED_MALE_FORENAME);
+        Assertions.assertNotEquals(StringHelper.EMPTY, name);
+        Assertions.assertNotEquals(Database.DEFAULT_VALUE, name);
+    }
+
+    @Test
+    public void getDefaultName() throws Exception {
+        Assertions.assertEquals(Constant.DEFAULT_NAME, NameGenerator.getDefaultName());
+    }
+}
