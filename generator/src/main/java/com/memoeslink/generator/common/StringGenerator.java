584410174b95e0856acd6d24c97b04b6ba94a211
memoeslink <memoeslink@hotmail.com>
2023-11-09 17:20:29 -0600

    Refactored code for parity.

--- generator/src/main/java/com/memoeslink/generator/common/StringGenerator.java
+++ generator/src/main/java/com/memoeslink/generator/common/StringGenerator.java
@@ -14,16 +14,29 @@ public class StringGenerator extends Generator {
         super(locale, seed);
     }
 
-    public String getPercentage() {
-        return r.getInt(101) + "%";
+    public String getPercentage(boolean decimal) {
+        float number = r.getFloatInRange(0.0F, 100.0F);
+
+        if (decimal)
+            return String.format(locale, "%.2f%%", number);
+        return Math.round(number) + "%";
     }
 
-    public String getDecimalPercentage() {
-        return String.format(locale, "%.2f%%", r.getFloatInRange(0.0F, 100.0F));
-    }
+    public String getAlphanumeric(int length, boolean uppercaseAllowed) {
+        length = IntegerHelper.defaultByRange(length, 0, 9999);
+        StringBuilder sb = new StringBuilder();
+        String letters = com.memoeslink.generator.base.Constant.LOWERCASE_ALPHABET;
 
-    public String getHexColor() {
-        return String.format("#%06x", r.getInt(0xFFFFFF + 1));
+        if (uppercaseAllowed)
+            letters = com.memoeslink.generator.base.Constant.LETTERS;
+
+        for (int n = 0; n < length; n++) {
+            if (r.getBoolean())
+                sb.append(ResourceGetter.with(r).getChar(letters));
+            else
+                sb.append(r.getInt(10));
+        }
+        return sb.toString();
     }
 
     public String getPassword(int length) {
@@ -34,5 +47,9 @@ public class StringGenerator extends Generator {
             sb.append(ResourceGetter.with(r).getChar(Constant.PASSWORD_CHARS));
         }
         return sb.toString();
+    }
+
+    public String getHexColor() {
+        return String.format("#%06x", r.getInt(0xFFFFFF + 1));
     }
 }
